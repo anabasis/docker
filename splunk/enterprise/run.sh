8e615ee92d2e163998147095bc3d86e03187bfc8
anabasis <anabasis01@gmail.com>
2018-10-21 17:37:53 +0900

    gitignore

--- splunk/enterprise/run.sh
+++ splunk/enterprise/run.sh
@@ -3,7 +3,7 @@ export SPLUNK_CONTAINER_NAME="splunkpublic"
 docker \
   run \
   --detach \
-  --volume /Users/chojunseung/Workings/Containers/splunk/splunk_repo/splunk_etc:/opt/splunk/etc \
+  --volume /Users/chojunseung/Workings/Repos/git/splunk_app:/opt/splunk/etc/apps \
   --name ${SPLUNK_CONTAINER_NAME} \
   --hostname splunkpublic.localdomain \
   --env "SPLUNK_START_ARGS=--accept-license  --seed-passwd welcome!1 " \
